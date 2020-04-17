3cf8ec0681f66ab242bbe889d0b879acd39d8bed
v-v-d <v-v-d@bk.ru>
2020-04-17 23:09:56 +0300

    token validation code added

--- AlbumsBackend/albums_backend/token_validation.py
+++ AlbumsBackend/albums_backend/token_validation.py
@@ -15,12 +15,15 @@ def decode_token(token, public_key):
     return jwt.decode(token, public_key, algoritms='RS256')
 
 
-def generate_token_header(username, private_key):
-    '''
-    Generate a token header base on the username. Sign using the private key.
-    '''
+def generate_token_header(username, user_id, private_key):
+    """
+    Generate a token header base on the user info. Sign using the private key.
+    """
     payload = {
-        'username': username,
+        'user': {
+            'name': username,
+            'id': user_id,
+        },
         'iat': datetime.utcnow(),
         'exp': datetime.utcnow() + timedelta(days=2),
     }
@@ -30,12 +33,12 @@ def generate_token_header(username, private_key):
 
 
 def validate_token_header(header, public_key):
-    '''
+    """
     Validate that a token header is correct
 
     If correct, it return the username, if not, it
     returns None
-    '''
+    """
     if not header:
         logger.info('No header')
         return None
@@ -45,7 +48,9 @@ def validate_token_header(header, public_key):
     if not parse_result:
         logger.info(f'Wrong format for header "{header}"')
         return None
+
     token = parse_result[0]
+
     try:
         decoded_token = decode_token(token.encode('utf8'), public_key)
     except jwt.exceptions.DecodeError:
@@ -56,15 +61,26 @@ def validate_token_header(header, public_key):
         logger.error(f'Authentication header has expired')
         return None
 
-    # Check expiry is in the token
-    if 'exp' not in decoded_token:
-        logger.warning('Token does not have expiry (exp)')
+    if not is_dict_keys_valid(('exp', 'user'), decoded_token, 'decoded_token'):
         return None
 
-    # Check username is in the token
-    if 'username' not in decoded_token:
-        logger.warning('Token does not have username')
+    user = decoded_token['user']
+    if not isinstance(user, dict):
+        logger.warning(f'Wrong user type. Expected: dict, got: {type(user)}')
+        return None
+
+    if not is_dict_keys_valid(('name', 'id'), user, 'user'):
         return None
 
     logger.info('Header successfully validated')
-    return decoded_token['username']
+    return user
+
+
+def is_dict_keys_valid(expected_keys, dict_obj, obj_name):
+    if not all(key in dict_obj for key in expected_keys):
+        logger.warning(
+            f'Wrong {obj_name} keys in token. '
+            f'Expected: {", ".join(expected_keys)}, got: {", ".join(dict_obj.keys())}'
+        )
+        return False
+    return True
