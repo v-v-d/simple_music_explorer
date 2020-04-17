b00ce5b41b62d555cf7d4411238047bcb24918b7
v-v-d <v-v-d@bk.ru>
2020-04-17 23:09:37 +0300

    albums controllers and models code added

---
+++ AlbumsBackend/albums_backend/api_namespaces.py
@@ -0,0 +1,56 @@
+from flask_restplus import Namespace, fields
+
+
+api_namespace = Namespace('api', description='Albums microservice API operations')
+
+auth_parser = api_namespace.parser()
+auth_parser.add_argument(
+    'Authorization', location='headers', type=str,
+    help='Bearer Access Token'
+)
+
+album_parser = auth_parser.copy()
+album_parser.add_argument(
+    'name', type=str, required=True, help='Album name'
+)
+album_parser.add_argument(
+    'price', type=float, required=True, help='Album price'
+)
+album_parser.add_argument(
+    'genre', type=str, required=True, help='Album genre'
+)
+album_parser.add_argument(
+    'description', type=str, required=True, help='Album description'
+)
+
+song_parser = auth_parser.copy()
+song_parser.add_argument(
+    'name', type=str, required=True, help='Song name'
+)
+song_parser.add_argument(
+    'album_id', type=int, required=True, help='Album id'
+)
+
+search_parser = api_namespace.parser()
+search_parser.add_argument(
+    'search', type=str, required=False, help='Search in the albums'
+)
+
+album_model = api_namespace.model('Album', {
+    'id': fields.Integer(),
+    'name': fields.String(),
+    'price': fields.Float(),
+    'genre': fields.String(),
+    'date': fields.DateTime(),
+    'description': fields.String(),
+    'artist_name': fields.String(),
+    'artist_id': fields.Integer(),
+})
+
+song_model = api_namespace.model('Song', {
+    'id': fields.Integer(),
+    'name': fields.String(),
+    'artist_name': fields.String(),
+    'artist_id': fields.Integer(),
+    'album_id': fields.Integer(),
+})
---
+++ AlbumsBackend/albums_backend/models.py
@@ -0,0 +1,37 @@
+from sqlalchemy import func
+
+from albums_backend.database import db
+
+
+class AlbumModel(db.Model):
+    __tablename__ = 'albums'
+    __table_args__ = (
+        db.CheckConstraint('price > 0'),
+    )
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Float, default=0)
+    genre = db.Column(db.String(32))
+    date = db.Column(db.DateTime, server_default=func.now())
+    description = db.Column(db.String(512))
+    artist_name = db.Column(db.String(128), nullable=False)
+    artist_id = db.Column(db.Integer, nullable=False)
+
+    songs = db.relationship(
+        'SongModel', foreign_keys='SongModel.album_id',
+        back_populates='album', cascade="all, delete-orphan"
+    )
+    # TODO: add a cover art
+
+
+class SongModel(db.Model):
+    __tablename__ = 'songs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    artist_name = db.Column(db.String(128), nullable=False)
+    album_id = db.Column(db.Integer, db.ForeignKey('albums.id'), nullable=False)
+    artist_id = db.Column(db.Integer, nullable=False)
+
+    album = db.relationship('AlbumModel', back_populates='songs')
